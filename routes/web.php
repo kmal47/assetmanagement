8c1d00846bc9c2ce430922640899013f9e945966
akmal <akmal.sharif4747@gmail.com>
2018-05-07 17:00:09 +0800

    tujuhmac

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'PagesController@index');
+
+Route::get('/about', function () {
+    return view('pages.about');
 });
+
